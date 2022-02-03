494fff755d97b97d9f09cb3f2a7b306c6f59620f
Belkıs Arslan <97946709+belkisarslan@users.noreply.github.com>
2022-02-03 14:21:38 +0300

    fal temel düzenleme

--- vanilla-js-wheel-of-fortune-part2-FINISHED/script.js
+++ vanilla-js-wheel-of-fortune-part2-FINISHED/script.js
@@ -1,8 +1,8 @@
 // Immediately invoked function expression
 // to not pollute the global scope
 (function() {
-  const wheel = document.querySelector('.wheel');
-  const startButton = document.querySelector('.button');
+  const img = document.querySelector('.img');
+  const button = document.querySelector('.btn');
   const display = document.querySelector('.display');
   
   let deg = 0;
@@ -10,14 +10,14 @@
 
   // Counter clockwise
   const symbolSegments = {
-    1: "Frog",
-    2: "Snail",
-    3: "Dolphin",
-    4: "Ladybug",
-    5: "Koala",
-    6: "Unicorn",
-    7: "Dragon",
-    8: "Snowman",
+    1: "Sana yol görünmüş balım. Alem duruşuna yollar gidişine hasta hadii bakiyiiiimmm",
+    2: "Bu ciddiyet sana fazla değil mi ponçik kuş? Hayatı bu kadar ciddiye almasan varyaaa nasıl uçarsın sen.",
+    3: "Hiçbir şey göremedim canım",
+    4: "Ay sen baya ballısın cicim. Kediler dört ayak üstüne düşür, sen onlardan da ballısın. Hayrını gör.",
+    5: "Üç vakte kadar sana gelen gelir haberin olsun.",
+    6: "Falın baya bi fallanmış. Öyle böyle bir fallanma değil o kadar diyim.",
+    7: "Bir daha çevir canım bu olmadı.",
+    8: "Sen varya müthişsin!",
   }
 
   const handleWin = (actualDeg) => {
@@ -25,34 +25,30 @@
     display.innerHTML = symbolSegments[winningSymbolNr];
   }
 
-  startButton.addEventListener('click', () => {
+  button.addEventListener('click', () => {
     // Reset display
-    display.innerHTML = "-";
+    display.innerHTML = " ";
     // Disable button during spin
-    startButton.style.pointerEvents = 'none';
+    button.style.pointerEvents = 'none';
     // Calculate a new rotation between 5000 and 10 000
     deg = Math.floor(5000 + Math.random() * 5000);
     // Set the transition on the wheel
-    wheel.style.transition = 'all 10s ease-out';
+    img.style.transition = 'all 5s ease-out';
     // Rotate the wheel
-    wheel.style.transform = `rotate(${deg}deg)`;
-    // Apply the blur
-    wheel.classList.add('blur');
+    img.style.transform = `rotate(${deg}deg)`;
   });
 
-  wheel.addEventListener('transitionend', () => {
-    // Remove blur
-    wheel.classList.remove('blur');
+  img.addEventListener('transitionend', () => {
     // Enable button when spin is over
-    startButton.style.pointerEvents = 'auto';
+    button.style.pointerEvents = 'auto';
     // Need to set transition to none as we want to rotate instantly
-    wheel.style.transition = 'none';
+    img.style.transition = 'none';
     // Calculate degree on a 360 degree basis to get the "natural" real rotation
     // Important because we want to start the next spin from that one
     // Use modulus to get the rest value
     const actualDeg = deg % 360;
     // Set the real rotation instantly without animation
-    wheel.style.transform = `rotate(${actualDeg}deg)`;
+    img.style.transform = `rotate(${actualDeg}deg)`;
     // Calculate and display the winning symbol
     handleWin(actualDeg);
   });
